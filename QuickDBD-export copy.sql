57b6f879e0f256ab20f6a79f5da956e38de8bf09
daisy287 <drodriguez2874@gmail.com>
2023-06-15 01:20:16 -0700

    new_work

---
+++ QuickDBD-export copy.sql
@@ -0,0 +1,62 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" text   NOT NULL,
+    "subcategory" text   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" text   NOT NULL,
+    "category" text   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "contact" (
+    "contact_id" integer   NOT NULL,
+    "first_name" text   NOT NULL,
+    "last_name" text   NOT NULL,
+    "email" text   NOT NULL,
+    CONSTRAINT "pk_contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" integer   NOT NULL,
+    "contact_id" integer   NOT NULL,
+    "company_name" text   NOT NULL,
+    "description" text   NOT NULL,
+    "goal" decimal   NOT NULL,
+    "pledged" decimal   NOT NULL,
+    "outcome" text   NOT NULL,
+    "backers_count" integer   NOT NULL,
+    "country" text   NOT NULL,
+    "currency" text   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" text   NOT NULL,
+    "subcategory_id" text   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contact" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
